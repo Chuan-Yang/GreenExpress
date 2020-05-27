90da852ec8fef1b79daf69928e688477c31aec61
chuan-yang <idreamyc@gmail.com>
2020-05-26 21:05:51 -0600

    Add left drawer

---
+++ green-express/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <LeftDrawer />
+  </div>
+</template>
+
+<script>
+import LeftDrawer from './LeftDrawer'
+
+  export default {
+    name: 'Home',
+    components: {
+      LeftDrawer
+    }
+  }
+</script>
